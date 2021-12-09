22051e955016b3d056caa13ea3236d950275c267
j-sohee <sohee.jin@gmail.com>
2021-12-09 22:11:07 +0900

    youtube layout 적용

--- js/youtube.js
+++ js/youtube.js
@@ -1,121 +1,67 @@
+const body = document.querySelector("body");
+const main = document.querySelector("#vidGallery");
+const key = "AIzaSyAAMk0RKq2aVlltRyS8dlW_Hr1zw8_ouCs";
+const playListId = "PL_R5V_bKthTtOskiIsA-w_VcyqRlaLdc0";
+const num = 8;
+const url = `https://www.googleapis.com/youtube/v3/playlistItems?key=${key}&part=snippet&playlistId=${playListId}&maxResults=${num}`;
 
-class Youtube{
-    constructor(option){
-        this.init(option);
-        this.bindingEvent();
-    }
-    
-    init(option){
-    
-        if(option.frame === undefined){
-            console.error("frame속성값은 필수 입력사항입니다.");
-            return;
-        }
-        if(option.key == undefined){
-            console.error("key값은 필수 입력값입니다.");
-            return;
-        }
-        if(option.playlist === undefined){
-            option.playlist = "PL_R5V_bKthTtOskiIsA-w_VcyqRlaLdc0"
-        }
-        if(option.num === undefined){
-            opt.num = 6;
-        }
-    
-        this.selector = option.frame;
-        this.playlist =  option.playList;
-        this.key = option.key;
-        this.num = option.num;
-    }
-    
-    bindingEvent(){
-        this.getYoutube();
-    
-        //썸네일 클릭시 팝업호출
-        $("body").on("click", this.selector+" article a", e=>{
-            e.preventDefault();
-            this.cratePop(e.currentTarget);
-        });
-    
-        $("body").on("click", ".vidpop span", ()=>{
-            $(".vidpop").remove();
-        })
-    }
-    
-    getYoutube(){
-        $.ajax({
-            url:"https://www.googleapis.com/youtube/v3/playlistItems",
-            dataType : "jsonp",
-            data : {
-                part : "snippet",
-                key : this.key,
-                maxResults : this.num,
-                playlistId : this.playlist
-            }
-        })
-        .success(data=>{
+fetch(url)
+.then(data=>{
+   return  data.json();
+})
+.then(json=>{
+    const items = json.items;
+
+    let result = "";
+    items.map(item=>{
+
+        let tit = item.snippet.title;
+        let desc = item.snippet.description;
+        let date = item.snippet.publishedAt.split("T")[0];
         
-            let items = data.items;
-        
-            $(items).each((_, data)=>{
-        
-                let txt = data.snippet.description; //본문
-                let tit = data.snippet.title; // 타이틀
-                let len = txt.length;
-                let titLen = tit.length;
-                let date = data.snippet.publishedAt;
-                date = date.split("T")[0];
-        
-                if( len >  70){
-                    txt = txt.substr(0, 80) + "...";
-                }
-                if( titLen > 50){
-                    tit = tit.substr(0, 50) + "...";
-                }
-        
-                $(this.selector)
-                    .append(
-                        $("<article>")
-                            .append(
-                                $("<a>").attr({ href : data.snippet.resourceId.videoId})
-                                        .append(
-                                            $("<img>").attr({ src : data.snippet.thumbnails.high.url })
-                                         ),
-                                $("<div class='btns'>")
-                                .append(
-                                    $("<a>").attr({ href : data.snippet.resourceId.videoId})
-                                            .text("VIEW")
-                                ),
-                                $("<div class='con'>")
-                                        .append(
-                                            $("<h3>").text(tit),
-                                            $("<p>").text(txt)
-                                        )
-                            )
-                    )
-            });
-        });
-        // .error(err=>{
-        //     console.error(err); 
-        // });
-    }
+        if(tit.length > 20) tit = tit.substr(0, 20)+"...";
+        if(desc.length > 100) desc = desc.substr(0, 100)+"...";
+        result += `
+            <article>
+                <a href="${item.snippet.resourceId.videoId}" class="pic">
+                    <img src="${item.snippet.thumbnails.standard.url}">
+                </a>
+                <div class="con">
+                    <h2>${tit}</h2>
+                    <p>${desc}</p>
+                    <span>${date}</span>
+                </div>
+            </article>
+        `;
+
+        main.innerHTML = result;
+    })
+})
+
+//썸네일 클릭시 팝업생성
+main.addEventListener("click", e=>{
+    e.preventDefault();
     
-    cratePop(item){
-        let vidId = $(item).attr("href");
-        
-        $("body")
-            .append(
-                $("<div class='vidpop'>")
-                    .append(
-                        $("<iframe>").attr({
-                            src : "https://www.youtube.com/embed/"+vidId,
-                            frameborder : 0,
-                            width:"100%",
-                            height: 600
-                        }),
-                        $("<span>").text("close")
-                    )
-            )
-    }
-}
+    if(e.target.parentElement.nodeName !== "A") return;
+    const vidId = e.target.closest("a").getAttribute("href");
+    console.log(vidId);
+    //console.log(e.target.parentElement.nodeName);
+
+    let pop = document.createElement("aside");
+    pop.innerHTML = `
+        <iframe src="https://www.youtube.com/embed/${vidId}" frameborder="0" width="100%" height="100%" allowfullscreen></iframe>
+        <span class="close">CLOSE</span>
+    `;
+
+    body.append(pop);
+});
+
+//팝업 닫기버튼 클릭시 팝업제거
+body.addEventListener("click", e=>{
+    const pop = body.querySelector("aside");
+    if(pop == null) return;
+
+    const close = pop.querySelector("span");
+    if(e.target == close) e.target.closest("aside").remove();
+})
 
